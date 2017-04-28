25e8736972dd76937e7a5c66116374766bf647b7
Sai Dulla <sai.dulla@berkeley.edu>
2017-04-28 08:36:09 -0700

    twelfth test

--- test/edu/berkeley/aep/BetweenNumbersTest.java
+++ test/edu/berkeley/aep/BetweenNumbersTest.java
@@ -34,5 +34,18 @@ public class BetweenNumbersTest {
         assertEquals(betweenNumbers.calculateBetweenNumbers(inputNumbers), outputNumbers);
 
     }
+    @Test
+    public void shouldReturnListOf15GivenInputList02346() throws Exception {
+        List<Integer> inputNumbers = new ArrayList<>();
+        inputNumbers.add(0);
+        inputNumbers.add(2);
+        inputNumbers.add(3);
+        inputNumbers.add(4);
+        inputNumbers.add(6);
+        List<String> outputNumbers = new ArrayList<>();
+        outputNumbers.add("1");
+        outputNumbers.add("5");
+        assertEquals(betweenNumbers.calculateBetweenNumbers(inputNumbers), outputNumbers);
+    }
 
 }
